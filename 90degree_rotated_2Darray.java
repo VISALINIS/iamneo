e2a65cd48f28b93ca9eca75b1c0fc97c8f5b7e91
Visalini S <svisalini0@gmail.com>
2026-06-09 00:02:13 +0530

    added

---
+++ 90degree_rotated_2Darray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+       
+        int n = sc.nextInt();
+        int[][] arr = new int[n][n];
+        
+        for(int i = 0;i<n;i++){
+            for(int j =0;j<n;j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+      
+        System.out.println("Rotated 2D Array:");
+        
+        for(int i = 0;i<n;i++){
+            for(int j =n-1;j>=0;j--){
+                System.out.print(arr[j][i]+" ");
+            }
+            System.out.println();
+        }
+        
+         
+        
+    }
+}
