85847cbe1d0135b9ac06d0cdfb475b2fd5826aad
Visalini S <svisalini0@gmail.com>
2026-06-11 22:55:26 +0530

    added

---
+++ pow_of_two.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        /*if(n<=0){
+            return false;
+        }
+        while(n%2==0){
+           n=n/2;
+        }*/
+        if (n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n%2!=0){
+            return false;
+        }
+        return isPowerOfTwo(n/2); 
+
+        }
+        
+    }
+    
+
